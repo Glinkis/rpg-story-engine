d07d3e2f03b52a12766ff9d7c606dd1706493841
Victor Glindås <glinkis@gmail.com>
2019-10-08 21:02:13 +0200

    Cleanup

--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,12 @@
 import express from "express"
-import { pool } from "../client/database"
 import * as enums from "./enums"
 
 const app = express()
 const port = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
-  res.send("Success!")
-})
-
-app.get("/enums", (req, res) => {
+  res.header("Content-Type", "application/json")
   res.send(JSON.stringify(enums, null, 2))
-})
-
-app.get("/db", async (req, res) => {
-  try {
-    const client = await pool.connect()
-    const result = await client.query("SELECT * FROM test")
-    res.send(result ? result.rows : null)
-    client.release()
-  } catch (error) {
-    res.send(error)
-  }
 })
 
 app.listen(port)
