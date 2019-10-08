176a806a1296f40c8e1449b08f3742c31c532202
Victor Glindås <glinkis@gmail.com>
2019-10-08 21:13:22 +0200

    Compress responses

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,19 @@
+import compression from "compression"
 import express from "express"
-import * as enums from "./enums"
 
 const app = express()
 const port = process.env.PORT || 5000
 
+const obj = {
+  key0: "value0",
+  key1: "value2",
+  key2: "value3",
+}
+
 app.get("/", (req, res) => {
   res.header("Content-Type", "application/json")
-  res.send(JSON.stringify(enums.WEALTH.POOR, null, 2))
+  res.send(JSON.stringify(obj, null, 2))
 })
 
+app.use(compression())
 app.listen(port)
