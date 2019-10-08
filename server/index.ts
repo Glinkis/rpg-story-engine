4a2974890c46d1c4858c56a72ad4495d14990033
Victor Glindås <glinkis@gmail.com>
2019-10-08 19:18:52 +0200

    Set up server

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!")
+import express from "express"
+import { Pool } from "pg"
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Success!")
+})
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+})
