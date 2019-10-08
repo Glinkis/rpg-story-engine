8ebf069018dd86703761c9723c0d68cb1f8689da
Victor Glindås <glinkis@gmail.com>
2019-10-08 21:09:19 +0200

    Return single value

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
   res.header("Content-Type", "application/json")
-  res.send(JSON.stringify(enums, null, 2))
+  res.send(JSON.stringify(enums.WEALTH.POOR, null, 2))
 })
 
 app.listen(port)
