7869504c89e93044ac5c69e57abe90283c459a20
Victor Glindås <glinkis@gmail.com>
2019-10-08 20:00:57 +0200

    Add database query

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,25 @@
 import express from "express"
+import { pool } from "../client/database"
 
 const app = express()
+const port = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
   res.send("Success!")
 })
 
-const port = process.env.PORT || 5000
+app.get("/db", async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query("SELECT * FROM test_table")
+
+    res.send(result ? result.rows : null)
+    client.release()
+  } catch (err) {
+    console.error(err)
+    res.send("Error " + err)
+  }
+})
 
 app.listen(port, () => {
   console.log(`server listening on port: ${port}`)
