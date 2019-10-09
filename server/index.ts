b486b0ace7e340d37db89bad8f523e4a9ada12f6
Victor Glindås <glinkis@gmail.com>
2019-10-09 17:37:37 +0200

    Update response

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,21 @@
 import compression from "compression"
 import express from "express"
+import * as alchemist from "./engine/alchemist"
+import * as blacksmith from "./engine/blacksmith"
 
 const app = express()
 
 // Compress responses.
 app.use(compression({ threshold: 8 }))
 
-const obj = {
-  key0: "value0",
-  key1: "value2",
-  key2: "value3",
-}
-
 app.get("/", (req, res) => {
   res.header("Content-Type", "application/json")
-  res.send(JSON.stringify(obj, null, 2))
+  res.write("Alchemist:\n")
+  res.write(JSON.stringify(alchemist))
+  res.write("\n\n")
+  res.write("Blacksmith:\n")
+  res.write(JSON.stringify(blacksmith))
+  res.end()
 })
 
 app.listen(process.env.PORT || 5000)
