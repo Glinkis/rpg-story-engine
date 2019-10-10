3de0c596b0456bb6493be0f16eece4e6a775b7e2
Victor Glindås <glinkis@gmail.com>
2019-10-10 13:08:07 +0200

    Use Biome type

--- server/engine/tools/contentsFetcher.ts
+++ server/engine/tools/contentsFetcher.ts
@@ -1,13 +1,14 @@
 import { randomValue } from "../rolls"
+import { Biome } from "../../../shared/types"
 
 interface Contents {
-  [key: string]: (town?: any, biome?: string, base?: any) => string
+  [key: string]: (town?: any, biome?: Biome, base?: any) => string
 }
 
 /**
  * This is the function used to handle random encounters. It needs a lot of work, and will change.
  */
-export function contentsFetcher(town: any, biome: string, keyTarget: string[], contents: Contents, base?: any) {
+export function contentsFetcher(town: any, biome: Biome, keyTarget: string[], contents: Contents, base?: any) {
   const key = randomValue(keyTarget)
   return contents[key](town, biome, base)
 }
