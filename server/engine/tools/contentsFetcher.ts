3bbc9fcf8880d8242234936155065e0bfe8eb32c
Victor Glindås <glinkis@gmail.com>
2019-10-10 12:05:48 +0200

    Update contentsFetcher

--- server/engine/tools/contentsFetcher.ts
+++ server/engine/tools/contentsFetcher.ts
@@ -1,12 +1,13 @@
-interface ContentsTarget {
-  [key: string]: (...args: any[]) => string
+import { randomValue } from "../rolls"
+
+interface Contents {
+  [key: string]: (town?: any, biome?: string, base?: any) => string
 }
 
 /**
  * This is the function used to handle random encounters. It needs a lot of work, and will change.
  */
-export function contentsFetcher(town: any, biome: string, keyTarget: any, contentsTarget: ContentsTarget, base?: any) {
-  const key = keyTarget.random()
-  const contents = contentsTarget[key](town, biome, base)
-  return contents
+export function contentsFetcher(town: any, biome: string, keyTarget: string[], contents: Contents, base?: any) {
+  const key = randomValue(keyTarget)
+  return contents[key](town, biome, base)
 }
