b08af03a3f5118a342cdf4179dd43255bd765307
Victor Glindås <glinkis@gmail.com>
2019-10-11 22:33:59 +0200

    Port factionsForType

---
+++ server/engine/tools/factionsForType.ts
@@ -0,0 +1,37 @@
+import { clone } from "../utils"
+import { randomValue } from "../rolls"
+import { createFaction } from "../factions/createFaction"
+
+/**
+ * Returns a random of factions for a leadership type.
+ *
+ * @param town The town object. Like most times it's passed as an argument, it's mandatory here.
+ *
+ * @param key The thing that we're searching for.
+ * This can be leadershipType, type, etc.
+ *
+ * @param value The value that is expected for variable.
+ * if it doesn't find any, it creates a matching faction.
+ * This is for plot hooks that require a wizard's college, etc.
+ */
+export function factionsForType(town, key: string, value: any) {
+  const found: string[] = []
+
+  for (const faction in town.factions) {
+    if (faction[key] === value) {
+      found.push(clone(faction))
+    }
+  }
+
+  if (found.length === 0) {
+    const tempFaction = createFaction(town, {
+      [key]: value,
+      isThrowaway: true,
+    })
+
+    town.factions[tempFaction.key] = tempFaction
+    found.push(tempFaction)
+  }
+
+  return randomValue(found)
+}
