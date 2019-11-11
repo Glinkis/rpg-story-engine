40b12fc46c007495073690f31e9461d515d12990
Victor Glindås <glinkis@gmail.com>
2019-11-11 18:38:26 +0100

    Fix quote formatting

--- shared/types.ts
+++ shared/types.ts
@@ -2,7 +2,7 @@ export type Roll = [number, ...(string | number)[]]
 
 export type RollData = Record<string, Roll[]>
 
-export type Biome = "desert" | "mountain" | "forest"
+export type Biome = `desert` | `mountain` | `forest`
 
 export interface Terrain {
   weather: Weather
