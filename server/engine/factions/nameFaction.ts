20fb55e620f11b2255eb129b6754a7186aa0e0e2
Victor Glindås <glinkis@gmail.com>
2019-11-12 21:20:05 +0100

    Refactor nameFaction

--- server/engine/factions/nameFaction.ts
+++ server/engine/factions/nameFaction.ts
@@ -8,14 +8,14 @@ export function nameFaction(town: string, type: string) {
   const data = factionData.type[type as FactionType]
   const { group, adjective, main, unique } = data
 
-  const name = randomValue([
+  const names = [
     rt`The ${group} of ${adjective} ${main}`,
     rt`The ${group} of ${main}`,
     rt`The ${adjective} ${group}`,
     rt`The ${main} of ${town}`,
     rt`The ${town} ${main}`,
     rt`${unique}`,
-  ])
+  ]
 
-  return name
+  return randomValue(names)
 }
