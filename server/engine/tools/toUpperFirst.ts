ca299c789a80238e45dc55533558f8c3ae6ed635
Victor Glindås <glinkis@gmail.com>
2019-11-13 20:52:03 +0100

    Add documentation comment

--- server/engine/tools/toUpperFirst.ts
+++ server/engine/tools/toUpperFirst.ts
@@ -1,3 +1,6 @@
+/**
+ * Converts the first letter in a text to uppercase.
+ */
 export function toUpperFirst(text: string) {
   if (typeof text === `string`) {
     return text.charAt(0).toUpperCase() + text.slice(1)
