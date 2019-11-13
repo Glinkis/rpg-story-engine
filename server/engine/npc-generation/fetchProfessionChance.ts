0b1759de97417e8e7e5f8ae92da10866b48ac8c6
Victor Glindås <glinkis@gmail.com>
2019-11-13 20:47:48 +0100

    Fix fetchProfessionChance

--- server/engine/npc-generation/fetchProfessionChance.ts
+++ server/engine/npc-generation/fetchProfessionChance.ts
@@ -7,24 +7,24 @@ import { professions } from "./professions"
  * This gets the starting profession.
  */
 export function fetchProfessionChance(town: Town, npc: NPC) {
-  const populations = Object.values(town.professions)
+  const populations = Object.entries(town.professions)
 
   const sum = npc.socialClass
-    ? populations.filter(profession => professions[profession].socialClass === npc.socialClass)
+    ? populations.filter(profession => professions[profession[1]].socialClass === npc.socialClass)
     : populations
 
-  const totalWeight = sum.reduce((total, value) => total + value)
+  const totalWeight = sum.reduce((total, value) => total + value[1], 0)
 
   let random = Math.floor(randomFloat(1) * totalWeight)
   let index = 0
 
   for (let i = 0; i < sum.length; i++) {
-    random -= sum[i]
+    random -= sum[i][1]
     if (random < 0) {
       index = i
       break
     }
   }
 
-  return professions[index]
+  return professions[populations[index][0]]
 }
