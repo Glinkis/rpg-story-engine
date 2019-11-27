c5d0b6ffba456a69649dedbbb1917c9815268269
Victor Glindås <glinkis@gmail.com>
2019-11-27 22:27:05 +0100

    Add emotion

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,27 @@
 import React, { useState } from "react"
+import styled from "@emotion/styled"
+
+const FancyButton = styled.button({
+  "margin": 32,
+  "padding": 32,
+  "border": `4px solid #014`,
+  "borderRadius": 32,
+  "backgroundColor": `#fefefe`,
+  "boxShadow": `2px 2px 24px rgba(3, 16, 48, 0.3)`,
+  "fontSize": 14,
+  "fontFamily": `sans-serif`,
+  "&:focus": {
+    outline: `none`,
+    borderColor: `#6cf`,
+  },
+})
 
 export function App() {
   const [state, setState] = useState(`Click me!`)
 
-  function onClick() {
+  const onClick = () => {
     setState(`Hydration works!`)
   }
 
-  return <button onClick={onClick}>{state}</button>
+  return <FancyButton onClick={onClick}>{state}</FancyButton>
 }
