2d767ad844551e0e95eac5291441769da544eb28
Victor Glindås <glinkis@gmail.com>
2020-01-13 20:16:15 +0100

    Update profile function

--- server/engine/npc-generation/profile.ts
+++ server/engine/npc-generation/profile.ts
@@ -1,9 +1,10 @@
 import { variables } from "../global"
 
 /**
- * this is a function that returns the profile widget for the provided object.
- * obj is the object. It is mandatory.
- * type is the type of object it is- it points towards npcs as a default.
+ * This is a function that returns the profile widget for the provided object.
+ * @param obj The object. It is mandatory.
+ * @param base Is the front-facing text.
+ * @param type Is the type of object it is- it points towards npcs as a default.
  * For buildings, point towards town.buildings.tavern
  * For factions, point towards town.factions
  */
@@ -15,8 +16,9 @@ export function profile(obj: any, base?: any, type?: string) {
     obj = variables[type][obj]
   }
 
-  // base is the user-facing text
-  base = base || obj.name || obj.race
-
-  return `<<profile \`$${type}[${JSON.stringify(obj.key)}] \`${JSON.stringify(base)}>>`
+  return JSON.stringify({
+    title: base || obj.name || obj.race,
+    type: type || obj.type,
+    key: obj.key || ``,
+  })
 }
