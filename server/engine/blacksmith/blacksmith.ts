840db1dcea3da9a885c9660ffce456f363448622
Victor Glindås <glinkis@gmail.com>
2020-01-18 19:35:28 +0100

    Fix broken type

--- server/engine/blacksmith/blacksmith.ts
+++ server/engine/blacksmith/blacksmith.ts
@@ -1,13 +1,15 @@
 import { AgeStage } from "../../../shared/AgeStage"
+import { MajorRace } from "../../../shared/Race"
+import { NPC } from "../npc-generation/npc"
 
-export interface Blacksmith {
+export interface Blacksmith extends NPC {
   key: any
   parentNoun: string
   siblingNoun: string
   niblingNoun: string
   childNoun: string
   ageStage: AgeStage
-  race: string
+  race: MajorRace
   firstName: string
   lastName: string
 }
